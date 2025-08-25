1b923164385e701d412496d94666b13d71be9c0e
cesar.acarapi <cesar21908@gmail.com>
2025-08-24 22:25:58 -0400

    Actualizacion de certificado v1.1

--- src/models/certificadoModel.js
+++ src/models/certificadoModel.js
@@ -25,12 +25,12 @@ const Certificado = {
   },
 
   update: async (id, data) => {
-    const { codigo, descripcion, fecha_emision, fecha_vencimiento, estado, usuario_modificacion } = data;
+    const { id_producto, codigo, descripcion, fecha_emision, fecha_vencimiento, estado, usuario_modificacion } = data;
     const query = `
       UPDATE certificado
       SET id_producto=$1, codigo=$2, descripcion=$3, fecha_emision=$4, fecha_vencimiento=$5, estado=$6, usuario_modificacion=$7, fecha_modificacion=NOW()
       WHERE id=$8 RETURNING *`;
-    const values = [codigo, descripcion, fecha_emision, fecha_vencimiento, estado, usuario_modificacion, id];
+    const values = [id_producto, codigo, descripcion, fecha_emision, fecha_vencimiento, estado, usuario_modificacion, id];
     const result = await pool.query(query, values);
     return result.rows[0];
   },
